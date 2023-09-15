d9cbfc4ae4bbb60bd577000c2bcbadb76ea7f09e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 16:17:06 +0330

    created UserManager model

--- src/users/models.py
+++ src/users/models.py
@@ -14,3 +14,26 @@ def phone_validator(phone:str):
     if not (matched := re.fullmatch(PHONE_REGEX_PATTERN, phone.strip())):
         raise ValidationError("Invalid phone number")
     return matched
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, phone, password, **other_fields):
+        if phone is None:
+            raise ValueError("Phone not given")
+
+        user = self.model(phone=phone, **other_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone, password, **other_fields)
