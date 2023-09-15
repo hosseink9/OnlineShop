ecf1d7c838afb1867cf6d87d60432636a9de7438
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 16:15:10 +0330

    add some package and phone validator func

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+import re
+
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ValidationError
+
+from core.models import BaseModel
+
+PHONE_REGEX_PATTERN = r"(((\+|00)(98))|0)?(?P<operator>9\d{2})-?(?P<middle3>\d{3})-?(?P<last4>\d{4})"
+
+def phone_validator(phone:str):
+    if not (matched := re.fullmatch(PHONE_REGEX_PATTERN, phone.strip())):
+        raise ValidationError("Invalid phone number")
+    return matched
