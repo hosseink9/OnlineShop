4ea064b86712754f6df065d74b59da00c52ad3e4
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 16:04:30 +0330

    Created Category model in product models

--- src/product/models.py
+++ src/product/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
