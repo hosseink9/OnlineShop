9e913a9fc202797ec42f415d9265286c0246eb1a
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 16:13:47 +0330

    Created Product model

--- src/product/models.py
+++ src/product/models.py
@@ -10,3 +10,15 @@ class Category(BaseModel):
 
     def __str__(self):
         return self.name
+
+
+class Product(BaseModel):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    quantity = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(null=True,blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
