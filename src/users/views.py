13c66101c28904c8ad7a2ace35430ca01aa1fe58
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 17:23:06 +0330

    Create LoginView in Views

--- src/users/views.py
+++ src/users/views.py
@@ -29,3 +29,36 @@ class RegisterView(APIView):
         user_serializer.is_valid(raise_exception=True)
         user_serializer.save()
         return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginView(APIView):
+
+    def post(self, request):
+        phone = request.data['phone']
+        password = request.data['password']
+
+        user = User.objects.filter(phone=phone).first()
+
+        if user is None:
+            raise AuthenticationFailed('User is not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Password is not correct')
+
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret',algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'jwt':token
+            }
+
+        update_last_login(None, user)
+        return response
