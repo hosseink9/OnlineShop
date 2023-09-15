81f9187901eca58fb34b97e05567a85f1c39b3e3
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 17:20:02 +0330

    Create OrderItem model

--- src/orders/models.py
+++ src/orders/models.py
@@ -22,3 +22,14 @@ class Order(BaseModel):
     customer = models.ForeignKey(User,on_delete=models.CASCADE)
     discount = models.ForeignKey(Discount,on_delete=models.SET_NULL)
     payment = models.ForeignKey(Payment,on_delete=models.CASCADE)
+
+
+class OrderItem(BaseModel):
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(decimal_places=2, max_digits=5)
+    discount = models.DecimalField(decimal_places=1, max_digits=3, default=0.0)
+
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+
+
