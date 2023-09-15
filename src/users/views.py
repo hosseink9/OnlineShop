6a1a08fd6a92d238757db9823042388d1220176a
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 17:21:12 +0330

    add some packages in views

--- src/users/views.py
+++ src/users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from django.contrib.auth.models import update_last_login
+from rest_framework.views import Response
+from django.contrib.auth import authenticate
+from django.utils import timezone
+from django.contrib.auth import login
+
+import random
+from datetime import timedelta
+import jwt,datetime
+
+from .models import User
+from .serializers import UserSerializer,UserVerifySerializer
 
 # Create your views here.
