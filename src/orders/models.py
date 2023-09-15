1ea2a94db4d15af7bbfe08eaa76a5ead75c54f91
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-15 17:19:01 +0330

    Create Discount and add some models

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+from users.models import User
+from product.models import Product
+
+class Discount(BaseModel):
+    discount = models.DecimalField(decimal_places=1, max_digits=3, default=0.0)
+
+    def __str__(self):
+        return self.discount
