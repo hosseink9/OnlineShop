82867c8e50b49016b76accb34b146b9d24f822b1
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-21 14:58:39 +0330

    Create JwtHelper class and add generate func

--- src/users/utils.py
+++ src/users/utils.py
@@ -3,3 +3,12 @@ from config import settings
 from rest_framework.views import Response
 from datetime import timedelta, datetime
 
+class JwtHelper:
+    @staticmethod
+    def generate_jwt_token(user_id, secret_key, expires_in_minutes):
+        payload = {
+            'user_id': user_id,
+            'exp': datetime.utcnow() + timedelta(minutes=expires_in_minutes)
+        }
+        return jwt.encode(payload, secret_key, algorithm='HS256')
+
