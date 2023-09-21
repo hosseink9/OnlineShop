ac63e196738ee2c01983f6090c11f6186ba8324e
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-21 14:51:34 +0330

    Add some packages and module

--- src/users/views.py
+++ src/users/views.py
@@ -1,27 +1,10 @@
-from rest_framework.views import Response, APIView
-from rest_framework import status
-from rest_framework.exceptions import AuthenticationFailed
-from django.contrib.auth.models import update_last_login
-from rest_framework.views import Response
-from django.contrib.auth import authenticate
-from django.utils import timezone
-from django.contrib.auth import login
-
-import random
-from datetime import timedelta
-import jwt,datetime
-
+from django.shortcuts import render
+from .serializers import SerializerRegisterUser, SerializerLogin, LoginOTPSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import User
-from .serializers import UserSerializer,UserVerifySerializer
-
-
-def generate_2fa(request):  # NOTE: This is not a view
-    request.session["2FA"] = random.randint(1000, 9999)
-    request.session["2fa_expire"] = (timezone.now() + timedelta(minutes=1)).strftime("%d/%m/%Y, %H:%M:%S")
-    print(f"generated:{request.session['2FA']}  until:{request.session['2fa_expire']}")
-    return request
-
-
+from .auth import JwtAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class RegisterView(APIView):
     def post(self, request):
