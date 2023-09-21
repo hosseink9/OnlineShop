d9ea5bac513448a016ce77f7ab0bb345cf4ca14d
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-21 14:44:59 +0330

    Create Register serializer

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,20 +6,20 @@ from django.utils.translation import gettext_lazy as _
 import random
 from datetime import timedelta
 from django.utils import timezone
+
+
+class SerializerRegisterUser(serializers.ModelSerializer):
     class Meta:
-        model = User
-        fields = ['username','first_name','last_name','email','phone','password']
+        model=User
+        fields=['email','phone','username','first_name','last_name','address']
 
-
-    def create(self, validate_data):
-        password = validate_data.pop("password",None)
-        instance = self.Meta.model(**validate_data)
-        if password is not None:
-            instance.set_password(password)
-        else:
-            raise ("You don't add password" )
-        instance.save()
-        return instance
+        def create(self, validated_data):
+            password=validated_data.pop('password', None)
+            instance=self.Meta.model(**validated_data)
+            if password is not None:
+                instance.set_password(password)
+            instance.save()
+            return instance
 
 class UserVerifySerializer(serializers.Serializer):
     otp = serializers.CharField(max_length=4)
