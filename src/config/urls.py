a1e06825fb7ae29d91e74bf596373690d85b5e49
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-22 12:20:24 +0330

    Add orders and product app urls in config urls

--- src/config/urls.py
+++ src/config/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
+    path('orders/', include('orders.urls')),
+    path('product/', include('product.urls')),
 ]
