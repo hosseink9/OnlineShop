4844c84245a0f209813786919d70c8251edb418f
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-22 12:27:00 +0330

    Create ShoppingCart View

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import Response, APIView
+from .models import Order
+from .serializers import OrderSerializer,OrderItemSerializer
 
-# Create your views here.
+
+class OrderItemCartView(APIView):
+    pass
+
+
+
+class ShoppingCartView(APIView):
+    def get(self,request):
+        pass
+
+    def post(self, request):
+        serializer_order=OrderSerializer(data=request.data)
+        serializer_order.is_valid(raise_exception=True)
+        print(serializer_order.data)
