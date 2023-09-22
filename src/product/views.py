7f1649d67006a8240ebfce8ef5e8c0f53e73dafa
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-22 12:22:38 +0330

    Add some modules and Create ProductList View

--- src/product/views.py
+++ src/product/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import filters
+from rest_framework import status
 
-# Create your views here.
+
+from .models import Product
+from .serializers import ProductListSerializer, SearchProductSerializer
+
+
+class ProductListView(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        if products:
+            serializer = ProductListSerializer(products, many=True)
+            return Response(serializer.data)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
