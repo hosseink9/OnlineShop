d7162bc0eb7908ade86c1caeb66c8d650db44f32
hosseink9 <hosseinkalantari.29@gmail.com>
2023-09-22 12:04:14 +0330

    Create Jwt authentication

--- src/users/auth.py
+++ src/users/auth.py
@@ -25,3 +25,22 @@ class UserAuthBackend(BaseBackend):
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+
+class JwtAuthentication(BaseAuthentication):
+
+    def authenticate(self, request):
+        auth_header=request.META.get("HTTP_AUTHORIZATION")
+        if not auth_header:
+            raise AuthenticationFailed
+        prefix,token=auth_header.split()
+        print(token)
+        if not prefix=='Bearer': #front set
+            raise AuthenticationFailed
+        user_id=JwtHelper.validate_jwt_token(token,SECRET_KEY)
+        if not user_id:
+            raise AuthenticationFailed
+        user=User.objects.get(id=user_id)
+        return user,token
+
